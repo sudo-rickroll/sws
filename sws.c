624dcfc10453aaf3617f5ceea3972e12b1173420
dstamout <dstamout@local>
2025-11-18 14:54:07 +0000

    Baseline input validation, needs more work

--- sws.c
+++ sws.c
@@ -1,13 +1,69 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "types.h"
 
-int
-main(int argc, char *argv[])
-{
-	(void)argc;
-	(void)argv;
-	printf("Hello, world!\n");
+int input_validation(int argc, char **argv, sws_options *config) {
+	/* Default assignments */
+	int opt;
+	struct stat st;
+	config->port = 8080;
+
+	while ((opt = getopt(argc, argv, "c:dhi:l:p:")) != -1) {
+		switch (opt) {
+			case 'c':
+				config->cgi = optarg;
+				break;
+			case 'd':
+				config->debug = true;
+				break;
+			case 'h':
+				config->help = true;
+				break;
+			case 'i':
+				config->address = optarg;
+				break;
+			case 'l':
+				config->log = optarg;
+				break;
+			case 'p':
+				/* There is a default assignment, but -p
+				 * still requires a port
+				 */
+				if (!optarg) {
+					fprintf(stderr, "Error: -p requires a port value\n");
+					return -1;
+				}
+				break;
+			/* Unknown, pass */
+			case '?':
+			default:
+				if (optopt == 'c' || optopt == 'i' || 
+					optopt == 'l' || optopt == 'p') {
+					fprintf(stderr, "Error: -%c requires an argument\n", optopt);
+				}
+				else if (isprint(optopt)) {
+					fprintf(stderr, "Error: unknown option '-%c'\n", optopt);
+				}
+				else {
+					fprintf(stderr, "Error: unknown option given\n");
+				}
+
+				return -1;
+		}
+	}
+
+
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	sws_options config;
+
+	input_validation(argc, argv, &config);
+
 	return EXIT_SUCCESS;
 }
--- types.h
+++ types.h
@@ -1,4 +1,16 @@
+#include <stdbool.h>
+
 #ifndef TYPES_H
 #define TYPES_H
+
+typedef struct {
+	char *cgi;
+	bool debug;
+	bool help;
+	char *address;
+	char *log;
+	unsigned port;
+	char *docroot;
+} sws_options;
 
 #endif
