735102b5f1f27f9a9615648ea3fbf0d7e1bc948f
dstamout <dstamout@local>
2025-11-23 15:00:13 +0000

    Minor update

--- sws.c
+++ sws.c
@@ -40,6 +40,7 @@ int input_validation(int argc, char **argv, sws_options *config) {
 			/* Unknown, pass */
 			case '?':
 			default:
+				/* Cases with arguments */
 				if (optopt == 'c' || optopt == 'i' || 
 					optopt == 'l' || optopt == 'p') {
 					fprintf(stderr, "Error: -%c requires an argument\n", optopt);
